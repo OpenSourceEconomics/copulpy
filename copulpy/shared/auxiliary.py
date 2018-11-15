727a3f9f014153df94dca6342f15309011e98c40
Felix Chopra <felix.chopra@googlemail.com>
2018-11-15 11:41:25 +0100

    Added spec builder.

--- copulpy/shared/auxiliary.py
+++ copulpy/shared/auxiliary.py
@@ -14,3 +14,21 @@ def distribute_copula_spec(copula_spec, *keys):
         rslt = rslt[0]
 
     return rslt
+
+
+def build_copula_spec(version, **kwargs):
+    """Build a copula spec from labeled arguments."""
+    spec = dict()
+    spec['version'] = version
+    spec[version] = dict()
+    spec[version]['version'] = version
+
+    if kwargs is not None:
+        for key, value in kwargs.items():
+            spec[version][key] = value
+
+    # Handle all optional arguments here
+    if (version == 'nonstationary') and ('unrestricted_weights' not in kwargs):
+        spec['nonstationary']['unrestricted_weights'] = None
+
+    return spec

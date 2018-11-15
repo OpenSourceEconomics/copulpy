66e5decbfa014866500edc7e028eed985f8fd08d
Felix Chopra <felix.chopra@googlemail.com>
2018-11-15 23:20:37 +0100

    Fixed regression test.

--- testing/run_regression.py
+++ testing/run_regression.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 """This module is a first take at regression tests."""
 import pickle as pkl
-
 import numpy as np
 
 from copulpy.tests.test_auxiliary import generate_random_request
@@ -38,12 +37,17 @@ if False:
 tests = pkl.load(open(PACKAGE_DIR + '/tests/regression_vault.copulpy.pkl', 'rb'))
 totaltests = len(tests)
 counter = 0
+
 for test in tests:
     counter += 1
+
     rslt, x, y, period, is_normalized, copula_spec = test
+    version = copula_spec['version']
     copula = UtilityCopulaCls(copula_spec)
 
     np.testing.assert_equal(copula.evaluate(
         x=x, y=y, t=period, is_normalized=is_normalized), rslt
     )
+
+    print('VERSION: {}.'.format(version))
     print('Test {0} of {1} passed.'.format(counter, totaltests))
